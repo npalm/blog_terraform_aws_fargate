aac16f7ba61bf6b32066fcbfb7ead5ca4cd19239
Niek Palm <dev.npalm@gmail.com>
2018-07-15 12:13:45 +0200

    Fix broken example, add ci

--- ci/bin/run-local.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-
-docker run --entrypoint="ci/bin/verify.sh" -it --rm -w /build -v $(pwd):/build hashicorp/terraform:0.11.7 fargate
-docker run --entrypoint="ci/bin/verify.sh" -it --rm -w /build -v $(pwd):/build hashicorp/terraform:0.11.7 fargate-ec2
